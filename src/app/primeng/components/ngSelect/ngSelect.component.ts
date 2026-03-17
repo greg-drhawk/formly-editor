134789f3d7c2301400bd5549511d618394ae2e17
Gregorio Giorgino <giorgino.greg@gmail.com>
2026-03-17 14:04:49 +0100

    fix ngselect

--- src/app/primeng/components/ngSelect/ngSelect.component.ts
+++ src/app/primeng/components/ngSelect/ngSelect.component.ts
@@ -2,11 +2,21 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FieldType, FieldTypeConfig, FormlyModule } from '@ngx-formly/core';
+import { SelectModule } from 'primeng/select';
+import { isObservable, Observable, of } from 'rxjs';
 
 @Component({
     selector: 'formly-field-ng-select',
     templateUrl: './ngSelect.component.html',
     styleUrl: './ngSelect.component.css',
-    imports: [ReactiveFormsModule, FormlyModule, CommonModule],
+    imports: [ReactiveFormsModule, FormlyModule, CommonModule, SelectModule],
 })
-export class FormlyFieldNgSelect extends FieldType<FieldTypeConfig> {}
+export class FormlyFieldNgSelect extends FieldType<FieldTypeConfig> {
+    options$!: Observable<any[]>;
+
+    ngOnInit() {
+        const opts = this.props.options;
+
+        this.options$ = isObservable(opts) ? opts : of(opts || []);
+    }
+}
